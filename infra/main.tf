b3c6dc0329a30c475952b5405c5092db183e22fd
Ayushman-1744 <ayushman1744@gmail.com>
2025-09-16 17:21:32 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -39,7 +39,7 @@ module "db" {
   env             = local.env
   vpc_id          = module.vpc.vpc_id
   public_subnets  = module.vpc.public_subnets
-  private_subnets = []      # if no private subnets
+  private_subnets = module.vpc.private_subnets # if no private subnets
   db_username     = var.db_username
   db_password     = var.db_password
   ec2_sg_id       = module.vpc.ec2_sg_id   # use SG created in VPC module
