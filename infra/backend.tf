52d363ef9a4b94a20dc24b771f25c6a90b502221
Ayushman-1744 <ayushman1744@gmail.com>
2025-09-18 15:48:03 +0530

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -5,7 +5,6 @@ terraform {
     key          = "envs/dev/terraform.tfstate"  # default workspace state
     region       = "ap-south-1"
     encrypt      = true
-  # use_lockfile = true
     dynamodb_table = "terraform-lock-table"
   }
 }
