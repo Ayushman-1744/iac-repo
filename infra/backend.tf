5061e15ba742e24e67f8a4dd4579be61121ab13d
Ayushman-1744 <ayushman1744@gmail.com>
2025-09-19 13:25:24 +0530

    backend.tf modified

--- infra/backend.tf
+++ infra/backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket       = "astro-1745"
-    key          = "envs/dev/terraform.tfstate"  # default workspace state
+    key          = "envs/terraform.tfstate"  # default workspace state
     region       = "ap-south-1"
     encrypt      = true
     dynamodb_table = "terraform-lock-table"
